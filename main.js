c6c76bdd129d0edc97263cf87ed48a22a958a71a
George Quarelli <quarelli.george@gmail.com>
2020-05-23 01:03:34 +0200

    little fix

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //al click sul pulsante fare partire una chiamata ajax a tmdb per recuperare i film che corrispondo alla query di ricerca inserita dall'utente.
 //Ciclare i risultati e per ogni film restituito, stamparne in pagina:
 //titolo,titolo originale,lingua,voto
+
 $(document).ready(function(){
   //intercetto il click sull'icona del cerca
   $('.icon').click(function() {
@@ -11,7 +12,9 @@ $(document).ready(function(){
       'method':'GET',
       'data':{
           'api_key':'7724b744f1b5d1ce3f3563c8cf9b4ea2',
-          'query': input_utente
+          'query': input_utente,
+          'language': 'it'
+
        },
        'success': function (data) {
          console.log(data);
