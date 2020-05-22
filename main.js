309f11348e6671cb7a564283440e8b629af3b2f3
George Quarelli <quarelli.george@gmail.com>
2020-05-23 00:41:07 +0200

    JS + intercetto clik + chiamata ajax

--- main.js
+++ main.js
@@ -2,10 +2,24 @@
 //Ciclare i risultati e per ogni film restituito, stamparne in pagina:
 //titolo,titolo originale,lingua,voto
 $(document).ready(function(){
-
+  //intercetto il click sull'icona del cerca
+  $('.icon').click(function() {
+  //creo una variabile per salvare l'input dell'utente
+  var input_utente = $('input').val();
     $.ajax({
-        'url' :
+      'url':'https://api.themoviedb.org/3/search/movie',
+      'method':'GET',
+      'data':{
+          'api_key':'7724b744f1b5d1ce3f3563c8cf9b4ea2',
+          'query': input_utente
+       },
+       'success': function (data) {
+         console.log(data);
+       },
+       'error': function () {
+         alert('si è verificato un errore');
+       }
 
-    });
-
-});
+     });
+   });
+  });
