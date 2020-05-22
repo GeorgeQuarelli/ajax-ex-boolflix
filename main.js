129ac0f2536884f18e887545136f7117e92c4e3e
George Quarelli <quarelli.george@gmail.com>
2020-05-22 19:27:04 +0200

    inserimento search bar +css + inizio ajax

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 //al click sul pulsante fare partire una chiamata ajax a tmdb per recuperare i film che corrispondo alla query di ricerca inserita dall'utente.
 //Ciclare i risultati e per ogni film restituito, stamparne in pagina:
 //titolo,titolo originale,lingua,voto
+$(document).ready(function(){
+
+    $.ajax({
+        'url' :
+
+    });
+
+});
