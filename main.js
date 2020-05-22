e2535f430d35d5ad0020429fb6c0aa7b2807f39c
George Quarelli <quarelli.george@gmail.com>
2020-05-22 18:26:21 +0200

    standard compile

---
+++ main.js
@@ -0,0 +1,3 @@
+//al click sul pulsante fare partire una chiamata ajax a tmdb per recuperare i film che corrispondo alla query di ricerca inserita dall'utente.
+//Ciclare i risultati e per ogni film restituito, stamparne in pagina:
+//titolo,titolo originale,lingua,voto
