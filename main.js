1856cbc2e147eb9af192558e1f6fd98f50179aff
George Quarelli <quarelli.george@gmail.com>
2020-05-25 15:12:03 +0200

    inserimento chiamata tv + telefilm (da fixare)

--- main.js
+++ main.js
@@ -7,37 +7,44 @@ $(document).ready(function(){
   //intercetto il tasto invio per la ricerca
   $('#search').keyup(function(event){
     if(event.which == 13) {
-    ricerca();
+    ricercalista();
     }
   });
 
   //intercetto il click sull'icona del cerca
   $('.icon').click(function() {
-    ricerca();
+    ricercalista();
 });
 
 
   //creo una funzione che include chiamata ajax e compilazione handlebars
-  function ricerca() {
-    //creo una variabile per salvare l'input dell'utente
-    //trim serve ad eliminare gli spazi prima e dopo all input utente
-    var input_utente = $('input').val().trim();
+  function ricercalista() {
+    //chiamata ajax film
+      chiamata_ajax("https://api.themoviedb.org/3/search/movie" , "film")
+    //chiamata ajax telefilm
+      chiamata_ajax("https://api.themoviedb.org/3/search/tv" , "telefilm")
+  }
 
-    //resetto l'input dell'utente
-    $('input').val('');
+    // funzione di generazione della chiamata ajax (film/telefilm)
+        function chiamata_ajax(url, genere) {
+          //creo una variabile per salvare l'input dell'utente
+          //trim serve ad eliminare gli spazi prima e dopo all input utente
+          var input_utente = $('input').val().trim();
 
-    //svuoto il contenitore dei risultati
-    $('.film-container.container').empty();
+          //resetto l'input dell'utente
+          $('input').val('');
 
-    //chiamata ajax dell'api
+          //svuoto il contenitore dei risultati
+          $('.film-container.container').empty();
+
+          //chiamata ajax
           $.ajax({
-            'url':'https://api.themoviedb.org/3/search/movie',
+            'url':url,
             'method':'GET',
             'data':{
                 'api_key':'7724b744f1b5d1ce3f3563c8cf9b4ea2',
                 'query': input_utente,
-                'language': 'it'
-
+                'language': 'it_IT'
              },
              'success': function (data) {
                var infofilm = data.results;
@@ -48,18 +55,28 @@ $(document).ready(function(){
                alert('devi insterire un film');
              }
            });
-      }
+        }
+
 
       //funzione per stampare le card dei film in pagina
-         function stampahtml(info_film) {
+         function stampahtml(info_film, genere) {
                  var schedafilm = $('#template').html();
                  var template_function = Handlebars.compile(schedafilm);
-
                  for (var i = 0; i < info_film.length; i++) {
                      var info= info_film[i];
+
+                     var titolo,titolo_originale;
+                      if (genere === "telefilm") {
+                       titolo = info.name;
+                       titolo_originale = info.original_name;
+                     } else {
+                         titolo = info.title;
+                         titolo_originale = info.original_title;
+                       }
+
                      var film = {
-                         'titolo-film': info.title,
-                         'titolo-original' : info.original_title,
+                         'titolo-film': titolo,
+                         'titolo-original' : titolo_originale,
                          'lingua': info.original_language,
                          'voto': info.vote_average,
                      }
@@ -67,5 +84,4 @@ $(document).ready(function(){
                      $('.film-container.container').append(html_finale);
                  }
                 }
-
   });
